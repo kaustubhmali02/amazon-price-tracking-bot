8ace66993b87fdaf090655acd96fff166925e1ef
Kaustubh Mali <kaustubhmali02@gmail.com>
2026-02-03 23:45:23 +0530

    fixes according to indian rupees

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import smtplib
 MY_ACCOUNT = os.environ['MY_ACCOUNT']
 MY_PASSWORD = os.environ['MY_PASSWORD']
 
-PRODUCT_URL = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
+PRODUCT_URL = "https://amzn.in/d/0fkMDJq3" # Brother HL-L2440DW Printer Link
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
@@ -20,11 +20,12 @@ response = requests.get(
     headers=headers)
 
 soup = BeautifulSoup(response.text, "lxml")
-product_price = float(soup.find(name="span", class_="a-offscreen").getText().strip("$"))
+product_price = float(soup.find(name="span", class_="a-offscreen").getText().strip("₹").replace(",",""))
 product_name = str(soup.find(name="span", id="productTitle").getText().strip())
-target_price = 270
+target_price = 12000
 
 if target_price >= product_price:
+    # print(f"{product_name}\n is now at ${product_price}\n")
     message = f"{product_name}\n is now at ${product_price}\n" \
               f"Link for purchase: {PRODUCT_URL}"
     em = EmailMessage()
